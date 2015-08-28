b79841ffa093ddeddf9bd498031afe25891e5a3c
Jacob Foard <jacobfoard@gmail.com>
2015-08-28 18:03:59 +0000

    Added Week 4 day 2 Challenges

--- week_4/Day_2/01_medium.php
+++ week_4/Day_2/01_medium.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <p>
-        <?
+        <?php
         /**
          * So we have our products, but what are we going to do with them.
          *
@@ -45,6 +45,258 @@
         ///////////////////////////
         // Put your code here!
         ///////////////////////////
+        interface Describable {
+            public function provideDescription(); 
+        }
+
+        abstract class Product implements Describable{
+          protected $name;
+          protected $price;
+          protected $brand;
+
+          public function __construct($name, $brand, $price){
+            $this->name = $name;
+            $this->brand = $brand;
+            $this->price = $price;
+          }
+          
+          abstract function provideDescriptionForProductType();
+          
+          public function provideDescription(){
+            return $this->provideDescriptionForProductType();
+          }
+          
+          public function getName(){
+            if(!(empty($this->name))){
+              return $this->name;
+            } else{
+            throw new Exception("Empty value found for name");
+            }
+          }
+          public function getPrice(){
+            if($this->price){
+              if(is_numeric($this->price)){
+                return $this->price;
+              } else{
+                throw new Exception("Invalid price");
+              }
+            } else {
+            throw new Exception("Empty value found for price");
+            }
+          }
+          public function getBrand(){
+            if(!(empty($this->brand))){
+              return $this->brand;  
+            } else {
+              throw new Exception("Empty value found for brand");
+            }
+          }
+        }
+        
+        class Television extends Product {
+          protected $displayType;
+          protected $size;
+          
+          public function __construct($name, $brand, $price, $displayType, $size){
+            parent::__construct($name, $brand, $price);
+            $this->displayType = $displayType;
+            $this->size = $size;
+          }
+          
+          public function getDisplayType(){
+            if(!(empty($this->displayType))){
+            return $this->displayType;
+            } else{
+              throw new Exception("Empty value found for display type");
+            }
+          }
+
+          public function getSize(){
+            if(!(empty($this->size))){
+            return $this->size;
+            } else{
+              throw new Exception("Empty value found for size");
+            }
+          }
+
+          public function provideDescriptionForProductType(){
+            try {
+            return "This is a " . $this->getSize() . " " . $this->getBrand() . " " . $this->getDisplayType() . " Television";
+            }
+            catch (Exception $e){
+              echo $e->getMessage();
+            }
+          }
+        }
+        
+        class Clothing extends Product {
+          protected $size;
+          protected $type;
+          protected $color;
+          protected $gender;
+          
+          public function getSize(){
+            if(!(empty($this->size))){
+            return $this->size;
+            } else{
+              throw new Exception("Empty value found for size");
+            }
+          }
+          public function getType(){
+            if(!(empty($this->type))){
+            return $this->type;
+            } else{
+              throw new Exception("Empty value found for type");
+            }
+          } 
+          public function getColor(){
+            if(!(empty($this->color))){
+              if(($this->color == "red") or ($this->color == "blue") or ($this->color == "green") or ($this->color == "black") or ($this->color == "white") or ($this->color == "yellow")){
+                  return $this->color;
+              } else{
+                throw new Exception("Invalid color");
+              }
+            } else{
+              throw new Exception("Empty value found for color");
+            }
+          }
+          public function getGender(){
+           if(!(empty($this->gender))){
+            return $this->gender;
+           } else {
+              throw new Exception("Empty value found for gender");
+            }
+          }
+
+          public function __construct($name, $brand, $price, $type, $size, $color, $gender){
+            parent::__construct($name, $brand, $price);
+            $this->size = $size;
+            $this->type = $type;
+            $this->color = $color;
+            $this->gender = $gender;
+          }
+          public function provideDescriptionForProductType(){
+            try{
+                return "This is an article of clothing. It is a " . $this->getBrand() . " " . $this->getColor() . " " . $this->getGender() . " " . $this->getType() . " of size " . $this->getSize() .  " it costs " . $this->getPrice(); 
+              }
+            catch(Exception $e){
+              echo $e->getMessage();
+            }
+          }
+        }
+
+        class CloudMovieFile implements Describable{
+            protected $title;
+            protected $director;
+            protected $genre;
+
+            public function __construct($title, $director, $genre){
+                $this->title = $title;
+                $this->director = $director;
+                $this->genre = $genre;
+            }
+
+            public function getTitle(){
+                return $this->title;
+            }
+            public function getDirector(){
+                return $this->director;
+            }
+            public function getGenre(){
+                return $this->genre;
+            }
+            public function provideDescription(){
+                return $this->getTitle() . " is a " . $this->getGenre() . " movie directed by " . $this->getDirector();
+            }
+        }
+
+        class ItemDescriber {
+          public function outputDescription($description){
+            if($description instanceof Describable){
+              return $description->provideDescription();
+            } else {
+              throw new Exception("Item is not describable");
+            }
+          }
+        }
+
+        class ShoppingCart implements Describable{
+            public $items = array();
+
+            public function provideDescription() {
+                foreach ($this->items as $item) {
+                   echo $item->provideDescription() . "</br>";
+                }
+            }
+
+            public function addProduct($product) {
+                if($product instanceof Product){
+                    $this->items[$product->getName()] = $product;
+                } else {
+                    throw new Exception("Error adding product, product is not a Product");
+                }
+            }
+
+            public function removeOne($product) {
+                if(in_array($product, $this->items)){
+                    unset($this->items[$product->getName()]);
+                } else {
+                    throw new Exception("Product is not in the cart");
+                }
+            }
+
+            public function removeAll() {
+                if(!(empty($this->items)) and is_array($this->item)){
+                    foreach ($this->items as $item) {
+                        $this->removeOne($item);
+                    }    
+                } else {
+                    throw new Exception("There are no items in the cart");
+                }
+            }
+
+            public function getTotalPrice() {
+                $total = 0;
+                
+                foreach ($this->items as $item) {
+                    $total = $item->getPrice() + $total;
+                }
+                return $total;
+            }
+
+            public function getAllProducts() {
+                return $this->items;
+            }
+
+            public function findProductByName($name) {
+                $itemKey = null;
+                foreach ($this->items as $key => $item) {
+                  if($name == $item->getName()){
+                    $itemKey = $key + 1;
+                    echo "$name is number $itemKey in the array";
+                    break;
+                  }
+                }
+                if($itemKey == null){
+                  throw new Exception("Item was not found in cart");
+                }
+            }
+        }
+
+        
+        $describer = new ItemDescriber();
+        $kramericaTV = new Television("Giant TV", "Kramerica", 3900.90, "LED", "100in");
+        $shirt = new Clothing("Button Down Shirt", "J Peterman", '29.88', "shirt","medium", "red", "male");
+        $cart = new ShoppingCart();
+        $cart->addProduct($kramericaTV);
+        $cart->addProduct($shirt);
+        $cart->provideDescription();
+        echo "</br>";
+        echo "The cart total price is $" . $cart->getTotalPrice();
+        $cart->removeOne($shirt);
+        $cart->findProductByName("Giant TV");
+        $describer->outputDescription($cart);
+
         ?>
     </p>
   </body>
